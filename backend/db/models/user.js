6d0cb0eb094d38a41a46618818c763c81a0f82da
Jesse Christensen <jtq.christensen@gmail.com>
2022-08-05 12:03:13 -0700

    Added user model functions

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,4 +1,8 @@
 'use strict';
+
+const { Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
 const {
   Model
 } = require('sequelize');
@@ -16,15 +20,27 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [3, 32],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) throw new Error('Username cannot be an email.')
+        } 
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [3, 256]
+      }
     },
     password: {
       type: DataTypes.STRING.BINARY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
     },
     access_level: {
       type: DataTypes.INTEGER,
@@ -33,10 +49,75 @@ module.exports = (sequelize, DataTypes) => {
     user_avatar: {
       type: DataTypes.TEXT,
       allowNull: true
+    },
+    display_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [0, 80]
+      }
     }
   }, {
+    defaultScope: {
+      attributes: {
+        exclude: ['email', 'password', 'createdAt', 'updatedAt', 'access_level']
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { 
+          exclude: ['password', 'createdAt', 'updatedAt']
+        }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    },
     sequelize,
     modelName: 'User',
   });
+
+  User.prototype.toSafeObject = function() {
+    const { id, username } = this;
+    return { id, username };
+  };
+
+
+  User.prototype.validatePassword = function(pw) {
+    return bcrypt.compareSync(pw, this.password.toString());
+  };
+
+
+  User.getCurrentUser = async function(id) {
+    return await User.scope('currentUser').findByPk(id);
+  };
+
+
+  User.login = async function({ username, password}) {
+
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.eq]: {
+          username
+        }
+      }
+    });
+
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
+  };
+
+
+  User.createUser = async function({ username, email, password }) {
+
+    const pwHash = bcrypt.hashSync(password);
+    const user = await User.create({ username, email, password: pwHash });
+
+    return await User.scope('currentUser').findByPk(user.id);
+  };
+
   return User;
 };
---
+++ backend/utils/validation/index.js
@@ -0,0 +1,47 @@
+const { check, validationResult } = require('express-validator');
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .trim().escape()
+    .isEmail().normalizeEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .trim()
+    .isLength({ min: 3, max: 32 })
+    .withMessage('Please provide a username with at least 3 characters and no more than 32.'),
+  check('username')
+    .isAlphanumeric()
+    .withMessage('Username must be alphanumeric only.'),
+  check('username')
+    .not().isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters.')
+];
+
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors
+      .array()
+      .map((error) => `${error.msg}`);
+
+    const err = Error('Bad request.');
+    err.errors = errors;
+    err.status = 400;
+    err.title = 'Bad request.';
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors,
+  validateSignup
+};
